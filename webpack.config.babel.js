d2bc35b99085a8a61fa36be29340d2500adb27d6
goper <goper@eetechmedia.com>
2017-06-06 17:09:19 +0800

    Setup sass with app.scss

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -14,7 +14,7 @@ module.exports = [
             filename: 'javascripts/app.js'
         },
         module: {
-            loaders: [
+            rules: [
                 { test: /\.js$/, use: 'babel-loader', exclude: /node_modules/ },
                 { test: /\.jsx$/, use: 'babel-loader', exclude: /node_modules/ },
             ]
@@ -28,13 +28,42 @@ module.exports = [
             filename: 'bootstrap.min.css'
         },
         module: {
-            loaders: [
-                { test: /\.(scss)$/, loader: 'css-loader!sass-loader!sass-loader?sourceMap'},
+            rules: [
+                {
+    				test: /\.scss$/,
+    				use: ExtractTextPlugin.extract({
+    		          fallback: "style-loader",
+    		          use: [ "css-loader", "sass-loader"]
+    		        })
+                },
                 { test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'file-loader' },
             ]
         },
         plugins: [
-            new ExtractTextPlugin("stylesheets/bootstrap/bootstrap.css"),
+            new ExtractTextPlugin("bootstrap.min.css"),
+        ]
+    },  {
+        entry: [
+            './resources/assets/sass/app.scss'
+        ],
+        output: {
+            path: path.resolve('./public/stylesheets'),
+            filename: 'app.min.css'
+        },
+        module: {
+            rules: [
+                {
+    				test: /\.scss$/,
+    				use: ExtractTextPlugin.extract({
+    		          fallback: "style-loader",
+    		          use: [ "css-loader", "sass-loader"]
+    		        })
+                },
+                { test: /\.(jpe|jpg|woff|woff2|eot|ttf|svg)(\?.*$|$)/, loader: 'file-loader' },
+            ]
+        },
+        plugins: [
+            new ExtractTextPlugin("app.min.css"),
         ]
     }
 ]
